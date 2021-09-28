f33ebc89b82f420626184edce939bcb89d49be60
Castillo <11235140+mrcastillo@users.noreply.github.com>
2021-09-28 02:08:41 -0400

    First work on website
    Started getting the HTML on board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className={"menu-container"}>
+        <div className={"menu-header"}>
+          <h1>galleria.</h1>
+        </div>
+        <div className={"menu-start-slide-wrapper"}>
+          <p>START SLIDE SHOW</p>
+        </div>
+      </div>
+
+      <div className={"galleria-container"}>
+        <div className={"galleria-image-box box250"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box400"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box285"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box250"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box340"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box280"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box500"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box250"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box420"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box260"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box420"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box260"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box330"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box525"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box340"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Starry Night</h1>
+            <h3>Vincent Van Gogh</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
