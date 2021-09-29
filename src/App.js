ccc91209170bdb26f3c0aa76ad3d928d742c2dc8
Castillo <11235140+mrcastillo@users.noreply.github.com>
2021-09-28 21:24:20 -0400

    Bug Or Feature?
    Had an unintended thing happen so im saving the code.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
       </div>
 
       <div className={"galleria-container"}>
-        <div className={"galleria-image-box box250"}>
+
+        <div className={"galleria-image-box box250 starry-night"}>
           
           <div className={"galleria-image-text"}>
             <h1>Starry Night</h1>
@@ -19,115 +20,115 @@ function App() {
           </div>
         </div>
 
-        <div className={"galleria-image-box box400"}>
+        <div className={"galleria-image-box box400 pearl-earring"}>
           
           <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
+            <h1>GIrl with a Pearl Earring</h1>
+            <h3>Johannes Vermeer</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box285 guernica"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Guernica</h1>
+            <h3>Pablo Picasso</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box250 magdalene"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Penitent Magdalene</h1>
+            <h3>Artemisia Gentileschi</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box340 galilee"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>The Storm on the Sea of Galilee</h1>
+            <h3>Rembrandt</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box280 kanagawa"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>The Great Wave off Kanagawa</h1>
+            <h3>Hokusai</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box500 van-gogh-self"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Van Gogh self-portrait</h1>
             <h3>Vincent Van Gogh</h3>
           </div>
         </div>
 
-        <div className={"galleria-image-box box285"}>
+        <div className={"galleria-image-box box250 sleeping-gypsy"}>
           
           <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
+            <h1>The Sleeping Gypsy</h1>
+            <h3>Henri Rousseau</h3>
+          </div>
+        </div>
+
+        <div className={"galleria-image-box box420 ermine"}>
+          
+          <div className={"galleria-image-text"}>
+            <h1>Lady with an Ermin</h1>
             <h3>Vincent Van Gogh</h3>
           </div>
         </div>
 
-        <div className={"galleria-image-box box250"}>
+        <div className={"galleria-image-box box260 night-cafe"}>
           
           <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
+            <h1>The Night Cafe</h1>
             <h3>Vincent Van Gogh</h3>
           </div>
         </div>
 
-        <div className={"galleria-image-box box340"}>
+        <div className={"galleria-image-box box420 basket-apples"}>
           
           <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
+            <h1>The Bakset of Apples</h1>
+            <h3>Paul Cezanne</h3>
           </div>
         </div>
 
-        <div className={"galleria-image-box box280"}>
+        <div className={"galleria-image-box box260 red-vest"}>
           
           <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
+            <h1>The Boy in the Red Vest</h1>
+            <h3>Edward Hopper</h3>
           </div>
         </div>
 
-        <div className={"galleria-image-box box500"}>
+        <div className={"galleria-image-box box330 arnolfini"}>
           
           <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
+            <h1>Arnolfini Portrait</h1>
+            <h3>Jan van Eyck</h3>
           </div>
         </div>
 
-        <div className={"galleria-image-box box250"}>
+        <div className={"galleria-image-box box525 mona-lisa"}>
           
           <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
+            <h1>Mona Lisa</h1>
+            <h3>Leonardo da Vinci</h3>
           </div>
         </div>
 
-        <div className={"galleria-image-box box420"}>
+        <div className={"galleria-image-box box340 the-swing"}>
           
           <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
-          </div>
-        </div>
-
-        <div className={"galleria-image-box box260"}>
-          
-          <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
-          </div>
-        </div>
-
-        <div className={"galleria-image-box box420"}>
-          
-          <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
-          </div>
-        </div>
-
-        <div className={"galleria-image-box box260"}>
-          
-          <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
-          </div>
-        </div>
-
-        <div className={"galleria-image-box box330"}>
-          
-          <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
-          </div>
-        </div>
-
-        <div className={"galleria-image-box box525"}>
-          
-          <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
-          </div>
-        </div>
-
-        <div className={"galleria-image-box box340"}>
-          
-          <div className={"galleria-image-text"}>
-            <h1>Starry Night</h1>
-            <h3>Vincent Van Gogh</h3>
+            <h1>The Swing</h1>
+            <h3>Jean-Honore Fragonard</h3>
           </div>
         </div>
       </div>
