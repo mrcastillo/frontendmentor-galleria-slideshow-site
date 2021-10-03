4dabeec779bbff28ed5b858cde504aeb44bf5cb9
Castillo <11235140+mrcastillo@users.noreply.github.com>
2021-10-02 22:02:14 -0400

    commit
    commit

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -5,13 +5,17 @@ import artist from "../assets/starry-night/artist.jpg";
 import previous from "../assets/shared/icon-back-button.svg";
 import next from "../assets/shared/icon-next-button.svg";
 
+import galleriaData from "../assets/data.json";
+
 function SlideShow() {
 
+    const currentGallery = galleriaData[0];
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
     
+
     return (
         <React.Fragment>
             <div className={"slide-show-container"}>
@@ -31,8 +35,8 @@ function SlideShow() {
 
                     <div className={"image-artist-title-header"}>
                         <div className={"image-artist-title-text"}>
-                            <h1>Starry Night</h1>
-                            <h3>Vincent Van Gogh</h3>
+                            <h1>{currentGallery.name}</h1>
+                            <h3>{currentGallery.artist.name}</h3>
                         </div>
                     </div>
                 </div>
@@ -43,21 +47,16 @@ function SlideShow() {
                     </div>
 
                     <div className={"slide-show-transparent-text"}>
-                        <p>1889</p>
+                        <p>{currentGallery.year}</p>
                     </div>
                 </div>
 
                 <div className={"slide-show-paragraph"}>
-                    <p>
-                        Although The Starry Night was painted during the day in Van Gogh's ground-floor studio, it would be inaccurate to state that the picture was painted from memory. 
-                        The view has been identified as the one from his bedroom window, facing east, a view which Van Gogh painted variations of no fewer than twenty-one times, 
-                        including The Starry Night. "Through the iron-barred window," he wrote to his brother, Theo, around 23 May 1889, "I can see an enclosed square of wheat 
-                        ... above which, in the morning, I watch the sun rise in all its glory.
-                    </p>
+                    <p>{currentGallery.description}</p>
                 </div>
 
                 <div className={"slide-show-source"}>
-                    <p>GO TO SOURCE</p>
+                    <a href={currentGallery.source}>GO TO SOURCE</a>
                 </div>
             </div>
 
@@ -68,8 +67,8 @@ function SlideShow() {
 
                 <div className={"slide-show-artist-title"}>
                     <div className={"slide-show-artist-title-header"}>
-                        <p id={"title"}>Starry Night</p>
-                        <p id={"artist"}>Vincent Van Gogh</p>
+                        <p id={"title"}>{currentGallery.name}</p>
+                        <p id={"artist"}>{currentGallery.artist.name}</p>
                     </div>
                     <div className={"slide-show-artist-title-next-previous"}>
                         <div className={"slide-show-artist-title-next-previous-icon"}>
