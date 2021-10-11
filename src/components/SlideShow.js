00c03a8e0ac9a037b586e66adaaac3b02a9e3a55
Castillo <11235140+mrcastillo@users.noreply.github.com>
2021-10-10 22:15:44 -0400

    Added Text To Speech
    Added Text to Speech and made Start SlideShow work

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,75 +1,103 @@
 import React, { useEffect, useState, useContext } from "react";
 import galleriaJSON from "../json/data.json";
 
+//SlideShow Context to check if the slideshow is currently active.
 import { SlideShowContext } from "./context/SlideShowContext";
+//Gallery Index to let the app understand which gallery image we are currently on or active.
 import { GalleriaIndexContext } from "./context/GalleriaIndexContext";
 
-function SlideShow(props) {
 
+function SlideShow(props) {
+    var galleriaTextToSpeech = new SpeechSynthesisUtterance();
+    galleriaTextToSpeech.lang = "en";
+    galleriaTextToSpeech.voice = window.speechSynthesis.getVoices[0];
+
+    //Set our context here.
     var { isSlideShowActive, setActiveSlideShow } = useContext(SlideShowContext);
     var { galleriaIndex, setGalleriaIndex } = useContext(GalleriaIndexContext);
 
-    //Gallery JSON with our data on current gallery.
+    //Set the current gallery, uses index to query through JSON in "galleriaJSON".
     var [ gallery, setGallery ] = useState(() => {
+        //If undefined state, set default state of 0
         if(typeof props.location.state === "undefined"){
             return galleriaJSON[0];
         }
-        setGalleriaIndex(props.location.state.galleryIndex);
+        setGalleriaIndex(props.location.state.galleryIndex)
+        //Also return and set the gallery.
         return galleriaJSON[props.location.state.galleryIndex];
     });
-
+    
+    galleriaTextToSpeech.text = gallery.description;
+    
+    //This determines the width of the <hr/> Element that we use to keep track of galleria progress. Mostly a CSS thing
     var [ playlistProgress, setPlaylistProgress ] = useState(()=> {
+        //Multiply the index by galleria length then multiply it by 100 to get an accurate 0-100% of where we are in the galleria.
         var playlistPercent = (galleriaIndex / galleriaJSON.length) * 100;
         return playlistPercent;
     });
 
+    //Open the overlay
     const openImage = () => {
         const slideOverlay = document.getElementsByClassName("slide-show-overlay");
         slideOverlay[0].style.width = "100%";
-    ;}
+    };
 
+    //Close the overlay
     const closeImage = () => {
         const slideOverlay = document.getElementsByClassName("slide-show-overlay");
         slideOverlay[0].style.width = "0%";
+        //Slideshow Stops once we close the image.
+        window.speechSynthesis.cancel()
         setActiveSlideShow(false);
     };
 
+    //Increments galleria by one and sets the next gallery image.
     const nextImage = () => {
         var nextGalleriaIndex = galleriaIndex + 1;
-
+        
         if(nextGalleriaIndex >= galleriaJSON.length) {
-            //window.scrollTo(0, 0);
             setActiveSlideShow(false);
             return null;
         } 
-
+        galleriaTextToSpeech.text = galleriaJSON[nextGalleriaIndex].description;
         setGallery(galleriaJSON[nextGalleriaIndex]);
         setGalleriaIndex(nextGalleriaIndex);
     };
 
+    //Decreases galleria increment by one and sets previous galleria image.
     const previousImage = (e) => {
         var previousGalleriaIndex = galleriaIndex - 1;
         if(previousGalleriaIndex < 0) {
-            //window.scrollTo(0, 0);
             return null;
         }
         setGallery(galleriaJSON[previousGalleriaIndex]);
         setGalleriaIndex(previousGalleriaIndex);
     };
 
+    const stopSlideShow = () => {
+        setActiveSlideShow(false);
+        window.speechSynthesis.pause();
+    }
+
     window.addEventListener("scroll", (e) => {
-        setActiveSlideShow(false)
+        setActiveSlideShow(false);
+        //window.speechSynthesis.cancel();
     });
 
+    galleriaTextToSpeech.onend = (event) => {
+        console.log("ended tts");
+        nextImage();
+        
+    }
+
     useEffect(() => {
-        window.scrollTo({
-            top: 0
-        });
+        setGallery(galleriaJSON[galleriaIndex]);
+        
+        window.scrollTo(0,0);
 
         var nextButton = document.getElementsByClassName("slide-show-artist-title-next-previous-icon")[1];
         if(galleriaIndex + 1 >= galleriaJSON.length) {
            nextButton.classList.add("disable");
-           setActiveSlideShow(false);
         } else {
             nextButton.classList.remove("disable");
         }
@@ -84,17 +112,20 @@ function SlideShow(props) {
         var playlistPercent = ((galleriaIndex + 1) / galleriaJSON.length) * 100;
         setPlaylistProgress(playlistPercent);
 
-        setGallery(galleriaJSON[galleriaIndex]);
-        
-        if(isSlideShowActive) {
+        galleriaTextToSpeech.text = gallery.description;
 
+        if(isSlideShowActive){
+            openImage();
+            window.speechSynthesis.speak(galleriaTextToSpeech);
+        } else {
+            //window.speechSynthesis.cancel();
         }
     }, [galleriaIndex, isSlideShowActive]);
 
     return (
         <React.Fragment>
             <div className={"slide-show-container"}>
-                <div className={"slide-show-overlay"} onClick={closeImage}>
+                <div className={"slide-show-overlay"} onClick={closeImage} onScroll={stopSlideShow}>
                     <div className={"slide-show-overlay-box"}>
                         <div onClick={closeImage} className={"slide-show-overlay-close"}>
                             <p>CLOSE</p>
